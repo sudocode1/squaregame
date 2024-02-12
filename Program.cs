180c13c2278646ddd3c43719e136cfac1f80c897
sudocode1 <roux@roux.pizza>
2024-02-12 02:50:24 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Raylib_cs;
+using Menus;
+
+namespace SquareGame
+{
+    class Program
+    {
+        public static bool running = true;
+        public static string currentMenu = "MainMenu";
+        public static void Main()
+        {
+            Raylib.InitWindow(800, 400, "test");
+
+            
+            while (running && !Raylib.WindowShouldClose()) 
+            {
+                // Raylib.BeginDrawing();
+                // Raylib.ClearBackground(Color.White);
+                // Raylib.DrawText("test", 14, 14, 20, Color.Black);
+                // Raylib.DrawText("test27", 35, 14, 20, Color.Black);
+                // Raylib.EndDrawing();
+
+                switch (currentMenu) {
+                    case "MainMenu":
+                        Menus.MainMenu.InputHandler(Raylib.GetKeyPressed());
+                        Menus.MainMenu.Render();
+                        break;
+                    
+                    default:
+                        Raylib.DrawText("fallback", 120, 55, 40, Raylib_cs.Color.Red);
+                        break;
+                }
+            }
+            
+            Raylib.CloseWindow();
+        }
+    }
+}
